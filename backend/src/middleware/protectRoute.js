6c8fd9bed7e84c80f8ba5d3264588cb51fff9f4e
Saurav Kumar Singh <sauravthespi@gmail.com>
2025-12-11 15:39:57 +0530

    full dev done

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -1,25 +1,26 @@
-import {  requireAuth } from '@clerk/express'
-import User from '../models/User.js';
+import { requireAuth } from "@clerk/express";
+import User from "../models/User.js";
 
-export const protectRoute =[
-    requireAuth({signInUrl: "/sign-in"}),
-    async(req,res,next)=>{
-        try{
-            const clerkId = req.auth().userId;
+export const protectRoute = [
+  requireAuth(),
+  async (req, res, next) => {
+    try {
+      const clerkId = req.auth().userId;
 
-            if(!clerkId)return res.status(401).json({message:"Unauthorized"});
+      if (!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token" });
 
-            const user = await User.findOne({clerkId});
-             
-            if(!user)return res.status(404).json({message:"User not found"});
+      // find user in db by clerk ID
+      const user = await User.findOne({ clerkId });
 
-            req.user = user;
-            next();
+      if (!user) return res.status(404).json({ message: "User not found" });
 
-        }catch(err){
-            console.error("Error in protectRoute middleware", err);
-            res.status(500).json({message:"Internal server error"});
-           
-        }
+      // attach user to req
+      req.user = user;
+
+      next();
+    } catch (error) {
+      console.error("Error in protectRoute middleware", error);
+      res.status(500).json({ message: "Internal Server Error" });
     }
-]
+  },
+];
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,38 +1,35 @@
-import express from 'express';
-import path from 'path';
-import { ENV } from './lib/env.js';
-import { connectDB } from './lib/db.js';
-import cors from 'cors';
-import { serve } from 'inngest/express';
-import { inngest,functions } from './lib/inngest.js';
-import { clerkMiddleware } from '@clerk/express'
-import chatRoutes from './routes/chatRoutes.js';
-import sessionRoutes from './routes/sessionRoute.js';
+import express from "express";
+import path from "path";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
 
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
 
+import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
-console.log(ENV.PORT)
+
 const __dirname = path.resolve();
 
+// middleware
 app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
-app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
 
-app.use("/api/inngest",serve({client:inngest , functions}));
-app.use(clerkMiddleware()); 
-app.use("/api/chat",chatRoutes)
-app.use("/api/sessions",sessionRoutes)
-
-
-app.get('/health', (req, res) => {
-  res.send('Hello World!');
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 });
 
-
-
-
-
+// make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -41,16 +38,12 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-
-
 const startServer = async () => {
-  try{
+  try {
     await connectDB();
-    app.listen(ENV.PORT, () => {
-      console.log(`Server is running on port ${ENV.PORT}`);
-    });
-  }catch(err){
-    console.log("Failed to start server", err);
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
   }
 };
 
