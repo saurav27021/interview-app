02a0ce3117f6f2793e7f2ac072d11ed46278c9ed
saurav <sauravthespi@gmail.com>
2026-02-17 02:01:24 +0530

    changes done

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,17 +1,19 @@
 import mongoose from 'mongoose';
 import { ENV } from './env.js';
 
+let isConnected = false;
+
 export const connectDB = async () => {
-    try{
-        if(!ENV.DB_URL){
+    if (isConnected) return;
+
+    try {
+        if (!ENV.DB_URL) {
             throw new Error("DB_URL is not defined in environment variables");
         }
-        const conn = await mongoose.connect(ENV.DB_URL)
-        console.log("connected to mongodb",conn.connection.host);
-
-    }catch(err){
-        console.log("Error connected mongodb",error);
-        process.exit(1);
-
+        const conn = await mongoose.connect(ENV.DB_URL);
+        isConnected = true;
+        console.log("connected to mongodb", conn.connection.host);
+    } catch (err) {
+        console.error("Error connecting to mongodb", err);
     }
 }
